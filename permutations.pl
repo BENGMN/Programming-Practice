35e1fd615eee828fc5481c2d0eaea6f8617f7c7e
Ben Crudo <ben.crudo@gmail.com>
2011-04-22 17:24:42 -0400

    Getting the last letter from the input string to the permutation generator

--- permutations.pl
+++ permutations.pl
@@ -1,11 +1,19 @@
 #! /usr/bin/perl -W
+use strict;
+use warnings;
+
 # The purpose of this script it to generate all permutations of a string
 
+# create a string to test with
+my $string = "abcd";
 
-$string = "abc";
+# call our function
+&getPerms($string);
 
 sub getPerms($) {
-
+  #first we'll get the last character from the string
+  my $last = substr($_[0], -1);
+  print $last."\n";
 }
 
 
