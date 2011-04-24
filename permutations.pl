545a7182aab865aba1d402a421bdd6849aea48d7
Ben Crudo <ben.crudo@gmail.com>
2011-04-24 03:41:54 -0400

    got an iterative approach going to building the permutations but am experiencing some off by one errors. Should probably fix this at a normal hour :)

--- permutations.pl
+++ permutations.pl
@@ -6,7 +6,15 @@ use warnings;
 ##########################################################################
 
 # 1) create a string to test with
-my $string = "abcdfg";
+my $string = "abc";
+
+print "Length of the string is ".length($string)."\n";
+print "This is the first value ".substr($string, 0, -2)."\n";
+print "This is the second value ".substr($string, 1, -1)."\n";
+print "This is the third value ".substr($string, 2, 1)."\n";
+
+print substr($string, 0, -3-0+1)."\n";
+
 
 # 2)  call our function
 &getPerms($string);
@@ -17,19 +25,44 @@ my $string = "abcdfg";
 ###################################
 
 sub getPerms($) {
-  # get the last character from the string
-  my $last = substr($_[0], -1);
 
-  # determine the length of the string
-  if ( length($_[0]) > 1 ) {
-    &getPerms(substr($_[0], 0, -1));
-  }
-  else {
-    print $_[0];
+  my %permutations; # we will use this hash to store the permutations 
+  my $length;
+
+  for (my $i = 0; $i < length($_[0]); $i++) {
+    # get all of the letters in the string 1 character at a time
+    # from left to right
+    #                       Start   End
+
+    $length = length($_[0]);           
+
+    if ($length - $i != 1) {
+     $length =  $i - $length + 1;
+    }
+    else {
+      $length = 1;
+    }
+
+    my $last = substr($_[0], $i, $length);
+    print "this is the last letter $last\n";
+ 
+    # this loop will start from the beginning of the string and
+    # go until the position of the letter we have stripped off
+    for (my $j = 0; $j < $i; $j++) {
+      $permutations {substr($_[0], 0, $j + 1).$last.substr($_[0],$j + 1, length($_[0]) - 1)} = $permutations{substr($_[0], 0, $j + 1).$last.substr($_[0], $j + 1, length($_[0]) - 1)} + 1;
+    }
+
+    # this loop will start from 1 after the position of the letter
+    # that we have stripped from the string and go until the end
+    for (my $k = $i+1; $k <= length($_[0]); $k++) {
+      $permutations {substr($_[0], 0, $k - 1).$last.substr($_[0],$k + 1, length($_[0]) - 1)} = $permutations{substr($_[0], 0,$k-1).$last.substr($_[0],$k+1, length($_[0]) - 1 )} + 1;
+    }
+  
   }
 
-#  print length($_[0])."\n";
-#  print $last."\n"; # debugging line
+  while ((my $key, my $value) = each(%permutations)) {
+     print $key."\n";
+  }
 }
 
 
