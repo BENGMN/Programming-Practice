90b0a277a50452a6ec366f5c4a40323ac2d064d6
Ben Crudo <ben.crudo@gmail.com>
2011-04-29 03:57:34 -0400

    Getting closer but still not yet complete. Succesfully going through the string now and inserting  a character after every letter although I need to generate more permutations based on switching the letters around as well

--- permutations.pl
+++ permutations.pl
@@ -7,14 +7,7 @@ use warnings;
 
 # 1) create a string to test with
 my $string = "abc";
-
 print "Length of the string is ".length($string)."\n";
-print "This is the first value ".substr($string, 0, -2)."\n";
-print "This is the second value ".substr($string, 1, -1)."\n";
-print "This is the third value ".substr($string, 2, 1)."\n";
-
-print substr($string, 0, -3-0+1)."\n";
-
 
 # 2)  call our function
 &getPerms($string);
@@ -32,10 +25,13 @@ sub getPerms($) {
   for (my $i = 0; $i < length($_[0]); $i++) {
     # get all of the letters in the string 1 character at a time
     # from left to right
-    #                       Start   End
 
-    $length = length($_[0]);           
+    # determine the length of the input string
+    $length = length($_[0]);
 
+    # since the length of the string is one larger than indexes into the string
+    # we set up a little logic to continuously get the proper string length 
+    # to provide to the substring command 
     if ($length - $i != 1) {
      $length =  $i - $length + 1;
     }
@@ -43,26 +39,44 @@ sub getPerms($) {
       $length = 1;
     }
 
-    my $last = substr($_[0], $i, $length);
-    print "this is the last letter $last\n";
- 
-    # this loop will start from the beginning of the string and
-    # go until the position of the letter we have stripped off
-    for (my $j = 0; $j < $i; $j++) {
-      $permutations {substr($_[0], 0, $j + 1).$last.substr($_[0],$j + 1, length($_[0]) - 1)} = $permutations{substr($_[0], 0, $j + 1).$last.substr($_[0], $j + 1, length($_[0]) - 1)} + 1;
-    }
+    my $char    = substr($_[0], $i  , $length);
+    my $prefix  = substr($_[0], 0   , $i);
+    my $postfix = substr($_[0], $i+1, length($_[0]) - $i);
+    my $short = $prefix.$postfix;
 
-    # this loop will start from 1 after the position of the letter
-    # that we have stripped from the string and go until the end
-    for (my $k = $i+1; $k <= length($_[0]); $k++) {
-      $permutations {substr($_[0], 0, $k - 1).$last.substr($_[0],$k + 1, length($_[0]) - 1)} = $permutations{substr($_[0], 0,$k-1).$last.substr($_[0],$k+1, length($_[0]) - 1 )} + 1;
+    print "Current Letter: $char \tprefix $prefix \tpostfix $postfix\n";
+ 
+    # this loop will start from the beginning of the prefix and
+    # go until the position of the letter we have stripped off
+    
+    # here we have another off by one error since the length of the string is 1 shorter than it used to be
+    for (my $j = 0; $j <= length($short); $j++) {
+      my $lhs = substr($short, 0, $j);
+      my $rhs = substr($short, $j, length($short) - $j);
+      my $perm;
+
+      if (length($lhs) == 0) {
+        $perm = $char.$short;
+      }
+      elsif (length($rhs) == 0) {
+        $perm = $short.$char;
+      }
+      else {
+        $perm = $lhs.$char.$rhs;
+      }
+
+      print "The permuatation is: $perm\t LHS: $lhs\tCHAR: $char\tRHS: $rhs\n";
+      $permutations {$perm} = 1;
+
     }
-  
   }
 
   while ((my $key, my $value) = each(%permutations)) {
      print $key."\n";
   }
+my $count = scalar keys %permutations;
+print $count."\n";
+
 }
 
 
